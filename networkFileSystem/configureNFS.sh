79e364818da22355d95eb66fb11d0092ad7ff312
radx64 <radek-team@wp.pl>
2015-03-02 22:05:11 +0100

    Rename network config script

--- networkFileSystem/configureNFS.sh
+++ networkFileSystem/configureNFS.sh
@@ -2,8 +2,20 @@
 nodesCount=4
 exportDirectory="/export/rpi/node"
 localDirectory="fs/node"
+interface="eth0"
+serverIp="10.0.0.254/24"
 #-------------------
 
+function reconfigureNetwork {
+	echo "Shutting down iterface "$interface" ..."
+	sudo ip link set dev $interface down
+	echo "Configuring server ip as "$serverIp" on "$interface
+	sudo ip addr add $serverIp dev $interface
+	echo "Starting interface "$interface" again ..."
+	sudo ip link set dev $interface up
+}
+
+function exportNFS {
 for node in `seq 1 $nodesCount`;
 do
 	dirname=$exportDirectory$node
@@ -19,3 +31,7 @@ do
 	echo $localdirname" to "$dirname
 	sudo mount --bind $localdirname $dirname
 done
+}
+
+reconfigureNetwork
+exportNFS
